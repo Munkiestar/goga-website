b907e93b0e4f06c732cd52d8d0d1e7dc88313324
Igor <munkieryder@gmail.com>
2022-02-15 21:21:24 +0100

    ADD google map + minor fixes

---
+++ src/components/map/Map.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Map.scss";
+
+function Map() {
+  return (
+    <section className="map">
+      <h1>GDJE SE NALAZIMO?</h1>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52372.61385559864!2d16.417546606367626!3d46.37937771300303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4768ae7fc00fee09%3A0x52af233921a182b!2sUl.%20Josipa%20Broza%2C%2040000%2C%20Ivanovec%2C%20Croatia!5e0!3m2!1sen!2sat!4v1644953286751!5m2!1sen!2sat"
+        id="Frame Example"
+        width="600"
+        height="450"
+        style={{ border: "none" }}
+        loading="lazy"
+      />
+
+      <section className="info">
+        <p>
+          <strong>Love by Goga</strong>
+        </p>
+        <p>
+          <strong>Ul. Josipa Broza, 40000, Ivanovec</strong>
+        </p>
+        <p>
+          <strong>Narudžbe:</strong> &nbsp;+385 99 802 0736
+        </p>
+        <p>
+          <strong>Email:</strong> &nbsp;lovebygoga@gmail.com
+        </p>
+      </section>
+    </section>
+  );
+}
+
+export default Map;
