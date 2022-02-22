df5fc1b12363a0790e5a68afaa28509f5c839a3c
Igor <munkieryder@gmail.com>
2022-02-22 20:26:53 +0100

    ADD home and navbar fix mobile

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,17 +5,14 @@ import home_img from "../../assets/home_page.jpg";
 function Home() {
   return (
     <main className="home">
-      <section className="home__main">
-        <div className="home__main-content">
-          <h1>Love by Goga</h1>
-          <p>
-            {" "}
-            Uljepšajte i uveselite vaš dom sa šarenim, unikatnim, ručno
-            oslikanim drvenim ukrasima
-          </p>
-        </div>
-      </section>
-      <img src={home_img} alt="" />
+      <div className="home__main-content">
+        <h1>Love by Goga</h1>
+        <p>
+          Uljepšajte i uveselite vaš dom sa šarenim, unikatnim, ručno oslikanim
+          drvenim ukrasima
+        </p>
+      </div>
+      <img src={home_img} alt="Love by Goga" />s
     </main>
   );
 }
