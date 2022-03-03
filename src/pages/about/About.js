f6f9b2ed85f135cab5d7b22be43c35b62c511f66
Igor <munkieryder@gmail.com>
2022-03-03 13:28:35 +0100

    about transition text remove

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -15,32 +15,23 @@ function About() {
       <img className="about__img" src={about_img} alt="Familija" />
 
       <p>
-        Oduvijek voli crtati, rezati, lijepiti… Još kao mala najviše vremena sam
-        provodila s bojicama i flomasterima u rukama pa bi se moglo reći da je
-        osnivanje kreativnog brenda Love by Goga bilo samo pitanje trenutka.
+        Oduvijek volim crtati, rezati, lijepiti… kreativka od malena, a Goran
+        spretan s alatom i sa puno strpljenja za moje ideje, pa je stvaranje
+        kreativnog brenda Love by Goga bilo samo pitanje vremena.
       </p>
       <p>
-        Oslikavam na drvetu, finoj iverici ili recikliranim paletama. U svemu mi
-        pomaže suprug Goran koji reže, brusi i hobla. Goran odrađuje ovaj
-        grublji, a ja finiji dio posla. Akrilnim bojama crtam, bojam i ukrašavam
-        daskice, a svaka od njih je unikatna. U ponudi imamo natpise za vrata i
-        zid, samostojeće natpise, slike i poruke na drvetu, police, okvire za
-        slike, rode, igračke i igrala, te sve ono što naši kupci požele a mi
-        možemo realizirati.
+        U početku smo radili daskice, kako ih od milja zovemo, za nas, naše
+        prijatelje i njihove prijatelje. Kada smo vidjeli da se one sviđaju i
+        ljudima koji nas ne poznaju i da pitaju kako doći do naših proizvoda
+        odlučili smo otvoriti obrt Bulović. Najviše nas vesele osmijeh i
+        zadovoljstvo na licima odraslih, a naročito djece kada u ruke prime
+        drvenu pločicu sa svojim imenom ili porukom koja je ispisana samo za
+        njih. Naše glave su prepuna ideja, inspiraciju pronalazimo svuda oko
+        sebe, a često nas oduševe zanimljive narudžbe kupaca koje se zatim
+        trudimo što bolje sprovesti u djelo.
       </p>
 
-      <div className={showMore ? "show div" : "hide"}>
-        <p>
-          U početku sam radila daskice, kako ih od milja zovemo, za sebe, svoje
-          prijatelje i njihove prijatelje. Kada sam vidjela da se one sviđaju i
-          ljudima koji me ne poznaju i da pitaju kako doći do mojih proizvoda
-          odlučila sam otvoriti obrt Bulović. Najviše me vesele osmijeh i
-          zadovoljstvo na licima odraslih, a naročito djece kada u ruke prime
-          drvenu pločicu sa svojim imenom ili porukom koja je ispisana samo za
-          njih. Moja glava je prepuna ideja, inspiraciju pronalazim svuda oko
-          sebe, a često me oduševe zanimljive narudžbe kupaca koje se zatim
-          trudim što bolje sprovesti u djelo.
-        </p>
+      <div className={showMore ? "show" : "hide"}>
         <p>
           Kada oslikavam svoje daskice pokušavam da se prvenstveno meni sviđa
           ono što sam napravila i da sam ja zadovoljna. Tek tada sam sigurna da
